d3eadb0472932b44ce978b7920a0cdf2632162f7
flomintv <flomintv@alfastrah.ru>
2018-10-10 10:12:32 +0300

    * Effective java's Item 1(Consider static factory methods instead of constructors) has been implemented

---
+++ src/main/java/staticcc/factory/method/man/Cleaner.java
@@ -0,0 +1,51 @@
+package staticcc.factory.method.man;
+
+public class Cleaner {
+
+    private Integer desiredCleanlinessLevel;
+
+    private static final Cleaner defaultCleaner = new Cleaner(0);
+
+    /**
+    * ctor
+    */
+    public Cleaner(Integer desiredCleanlinessLevel) {
+        this.desiredCleanlinessLevel = desiredCleanlinessLevel;
+    }
+
+    /**
+     * default factory
+     */
+    public static Cleaner defaultLevelCleaner() {
+        return defaultCleaner;
+    }
+
+    /**
+     * level 1 factory
+     */
+    public static Cleaner levelOneCleaner() {
+        return new Cleaner(1);
+    }
+
+    /**
+     * level 10 factory
+     */
+    public static Cleaner levelTenCleaner() {
+        return new Cleaner(10);
+    }
+
+    /**
+     * Tamara 'the cleaner' factory
+     */
+    public static Cleaner tamaraCleaner() {
+        return new Tamara(42);
+    }
+
+    /**
+     * actual cleaning action. well, sort of
+     */
+    public void clean() {
+        System.out.println(String.format("Let's pretend that I clean something. By the way, my cleanliness level is %d", desiredCleanlinessLevel));
+    }
+
+}
--- src/main/java/staticcc/factory/method/man/Main.java
+++ src/main/java/staticcc/factory/method/man/Main.java
@@ -3,7 +3,15 @@ package staticcc.factory.method.man;
 public class Main {
 
     public static void main(String[] args) {
+        Cleaner defaultLevelCleaner = Cleaner.defaultLevelCleaner();
+        Cleaner levelOneCleaner = Cleaner.levelOneCleaner();
+        Cleaner levelTenCleaner = Cleaner.levelTenCleaner();
+        Cleaner tamaraCleaner = Cleaner.tamaraCleaner();
 
+        defaultLevelCleaner.clean();
+        levelOneCleaner.clean();
+        levelTenCleaner.clean();
+        tamaraCleaner.clean();
     }
 
 }
